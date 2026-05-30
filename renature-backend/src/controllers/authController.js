ea17705fc278bce3d4144a808e255a8470d7692c
lucasborges04 <lucasbborges2004@gmail.com>
2026-05-30 17:27:33 -0300

    feat: adiciona login e logica do jwt

--- renature-backend/src/controllers/authController.js
+++ renature-backend/src/controllers/authController.js
@@ -1,11 +1,17 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 const authController = {
   async register(req, res) {
     try {
       const { name, email, password } = req.body;
 
-      // 2. Validação Básica
       if (!name || !email || !password) {
         return res.status(400).json({ message: "Preencha todos os campos." });
       }
@@ -30,9 +36,42 @@ const authController = {
           points: user.points,
           level: user.level,
         },
+        token: generateToken(user._id),
       });
     } catch (error) {
       console.error("Erro no cadastro:", error);
+      res.status(500).json({ message: "Erro interno no servidor." });
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({ message: "Preencha email e senha." });
+      }
+
+      const user = await User.findOne({ email });
+
+      if (user && (await user.matchPassword(password))) {
+        res.json({
+          message: "Login realizado com sucesso!",
+          user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            points: user.points,
+            level: user.level,
+            avatar: user.avatar,
+          },
+          token: generateToken(user._id),
+        });
+      } else {
+        res.status(401).json({ message: "Email ou senha inválidos." });
+      }
+    } catch (error) {
+      console.error("Erro no login:", error);
       res.status(500).json({ message: "Erro interno no servidor." });
     }
   },
