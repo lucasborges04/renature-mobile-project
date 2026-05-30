09d0577f9b1745189d55ff24de22ca8e29a9bccf
lucasborges04 <lucasbborges2004@gmail.com>
2026-05-30 12:43:47 -0300

    fix: atualiza funcao de salvar senha criptografada no banco de dados

--- renature-backend/src/models/User.js
+++ renature-backend/src/models/User.js
@@ -43,18 +43,13 @@ const UserSchema = new mongoose.Schema(
   },
 );
 
-UserSchema.pre("save", async function (next) {
+UserSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
-  try {
-    const salt = await bcrypt.genSalt(15);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.matchPassword = async function (enteredPassword) {
